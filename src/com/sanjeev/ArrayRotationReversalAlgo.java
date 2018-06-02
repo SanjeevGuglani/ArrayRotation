9057a5a1782f3df99329de27d0d3d4692da7e6b9
B0204071 <sanjeev.guglani@airtelbank.com>
2018-06-02 12:38:00 +0530

    ArrayRotationReversalAlgo

--- src/com/sanjeev/ArrayRotationReversalAlgo.java
+++ src/com/sanjeev/ArrayRotationReversalAlgo.java
@@ -3,5 +3,41 @@ package com.sanjeev;
 /**
  * Created by Sanjeev Guglani
  */
-public class ArrayRotationReversalAlgo {
+public class ArrayRotationReversalAlgo{
+
+        public static void arrayRotate(int arr[],int d){
+            reverseSubArray(arr,0,d-1);
+            reverseSubArray(arr,d,arr.length-1);
+            reverseSubArray(arr,0,arr.length-1);
+        }
+
+        public static void reverseSubArray(int arr[],int beg,int end){
+            int temp;
+            while(beg<end){
+                temp = arr[beg];
+                arr[beg]=arr[end];
+                arr[end]=temp;
+                beg++; end--;
+            }
+        }
+
+
+        /*UTILITY FUNCTIONS*/
+
+        /* function to print an array */
+        public  static void  printArray(int arr[]){
+            for(int i=0;i<arr.length;i++){
+                System.out.print(" "+arr[i]);
+            }
+            System.out.println();
+        }
+
+
+        // Driver program to test above functions
+        public static void main(String[] args) {
+            int arr[] = {1, 2, 3, 4, 5, 6};
+            arrayRotate(arr, 2);
+            printArray(arr);
+            System.out.println("Exit");
+        }
 }
