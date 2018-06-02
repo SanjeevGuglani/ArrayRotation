9c97bbe481dded2b63ca53503e6f3cd04229c27c
B0204071 <sanjeev.guglani@airtelbank.com>
2018-06-02 17:06:37 +0530

    Improved Printing Of Array

--- src/com/sanjeev/ArrayRotationReversalAlgo.java
+++ src/com/sanjeev/ArrayRotationReversalAlgo.java
@@ -1,5 +1,7 @@
 package com.sanjeev;
 
+import java.util.Arrays;
+
 /**
  * Created by Sanjeev Guglani
  */
@@ -26,9 +28,7 @@ public class ArrayRotationReversalAlgo{
 
         /* function to print an array */
         public  static void  printArray(int arr[]){
-            for(int i=0;i<arr.length;i++){
-                System.out.print(" "+arr[i]);
-            }
+            System.out.println(Arrays.toString(arr));
             System.out.println();
         }
 
